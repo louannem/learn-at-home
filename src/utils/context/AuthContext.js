fe8d3f8f9d230fcc9876858dc06287d179202ad6
louannem <84766775+louannem@users.noreply.github.com>
2022-05-05 21:27:53 +0200

    Created context to add user to the app

--- src/utils/context/AuthContext.js
+++ src/utils/context/AuthContext.js
@@ -1,10 +1,24 @@
-import React from "react"
+import React, { useContext, useState } from "react"
 
 const AuthContext = React.createContext()
 
-export const AuthProvider = () => {
+//Hook to access context
+export function useAuth() {
+    return useContext(AuthContext)
+}
+
+//Context provider
+export const AuthProvider = ({children}) => {
+    //No user by default
+    const [currentUser, setCurrentUser ] = useState()
+    
+    //Context data/info
+    const value = {
+        currentUser
+    }
     return(
-        <>
-        </>
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
     )
 } 
